718a946db8995d0bcef7762e5ac89b6b5b7e6629
aleuc <aleuc0099@gmail.com>
2024-05-11 01:57:37 -0300

    style iniciado

---
+++ scripts/interactive_page.js
@@ -0,0 +1,13 @@
+// Página interactiva creada por Alejo Cifarelli
+
+const info_box = document.querySelector("#more_info");
+const p_one = document.querySelector("#p_one");
+const p_two = document.querySelector("#p_two");
+const github_link = document.createElement("a");
+info_box.appendChild(github_link);
+let link = document.createTextNode("GitHub");
+github_link.appendChild(link);
+github_link.href = "https://github.com/aleuc0099";
+
+p_one.textContent = "Esta página interactiva fue creada por Alejo Cifarelli en el año 2024";
+p_two.innerHTML = "Para más información, visitar";
